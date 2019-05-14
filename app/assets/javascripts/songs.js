33bda469bc46a6da6ed87d4ed4f94e882aa5f09a
Marc Mills <mdmills1990@gmail.com>
2019-05-14 13:07:41 -0500

    building fetch js function for songs index

--- app/assets/javascripts/songs.js
+++ app/assets/javascripts/songs.js
@@ -3,8 +3,15 @@ $(() => {
 })
 
 const hoverHandlers = () => {
-  $("#songs-index").hover(function(e){
-    console.log(e);
-    console.log("hey it's working!")
+  $("#songs-index").on('click', (e) => {
+    e.preventDefault();
+    fetch(`/songs.json`)
+      .then(res => res.json())
+      .then(songs => {
+        $("#app-container.wrapper").html('')
+        songs.forEach((song) => {
+          console.log(song)
+      })
+    })
   });
-};
+}
