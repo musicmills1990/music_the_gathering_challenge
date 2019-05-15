2640ce4f1bcf9c87c2624d2321c9f9a48f24a37c
Marc Mills <mdmills1990@gmail.com>
2019-05-15 13:31:59 -0500

    experimenting with repainting DOM, songgs index page with new song form

--- app/assets/javascripts/characters.js
+++ app/assets/javascripts/characters.js
@@ -69,6 +69,57 @@ Character.prototype.formatShow = function(){
     <li>${this.songs}</li>
     <p> how do I iterate over the character's songs for this? </p>
     <a href="/characters/${this.id}/comments">${this.name}'s Comment Page</a>
+
+    <form class="new_song" id="new-song-form" action="/songs" accept-charset="UTF-8" method="post"><input name="utf8" type="hidden" value="✓"><input type="hidden" name="authenticity_token" value="8sd+mF+xEuGupYcksszGv2THi6g/iIMQ36YPGRRRVDoUj2h+HY3mCZi54kPZqd3cNkfqOk0saFWYCLcfch2R8Q==">
+      <label for="song_Song_Name">Song name</label>
+      <input type="text" name="song[name]" id="song_name"><br>
+
+      <label>Tune (instrumnetal only) or Song?</label>
+      <select name="song[category]" id="song_category"><option value="Tune">Tune</option>
+    <option value="Song">Song</option></select><br>
+      <label for="song_Characters who know this song:">Characters who know this song:</label><br>
+        <input type="hidden" name="song[character_ids][]" value="">
+          <label for="song_character_ids_79">Azia</label>
+          <input type="checkbox" value="79" name="song[character_ids][]" id="song_character_ids_79"><br>
+
+          <label for="song_character_ids_80">Liv</label>
+          <input type="checkbox" value="80" name="song[character_ids][]" id="song_character_ids_80"><br>
+
+          <label for="song_character_ids_81">Marco</label>
+          <input type="checkbox" value="81" name="song[character_ids][]" id="song_character_ids_81"><br>
+
+          <label for="song_character_ids_82">Daniel</label>
+          <input type="checkbox" value="82" name="song[character_ids][]" id="song_character_ids_82"><br>
+
+          <label for="song_character_ids_83">Tonya</label>
+          <input type="checkbox" value="83" name="song[character_ids][]" id="song_character_ids_83"><br>
+
+          <label for="song_character_ids_84">Dylan</label>
+          <input type="checkbox" value="84" name="song[character_ids][]" id="song_character_ids_84"><br>
+
+          <label for="song_character_ids_85">Franka</label>
+          <input type="checkbox" value="85" name="song[character_ids][]" id="song_character_ids_85"><br>
+
+          <label for="song_character_ids_86">Droolin Jack</label>
+          <input type="checkbox" value="86" name="song[character_ids][]" id="song_character_ids_86"><br>
+
+          <label for="song_character_ids_87">Mad Molly</label>
+          <input type="checkbox" value="87" name="song[character_ids][]" id="song_character_ids_87"><br>
+
+          <label for="song_character_ids_88">Lady Victoria</label>
+          <input type="checkbox" value="88" name="song[character_ids][]" id="song_character_ids_88"><br>
+
+          <label for="song_character_ids_89">Lady Morgan</label>
+          <input type="checkbox" value="89" name="song[character_ids][]" id="song_character_ids_89"><br>
+
+          <label for="song_character_ids_90">Lady Rosaline</label>
+          <input type="checkbox" value="90" name="song[character_ids][]" id="song_character_ids_90"><br>
+
+          <label for="song_character_ids_91">Donna Catalina</label>
+          <input type="checkbox" value="91" name="song[character_ids][]" id="song_character_ids_91"><br>
+      <input type="submit" name="commit" value="Add Song" data-disable-with="Add Song">
+    </form>
+
   `
   return characterHtml
 }
